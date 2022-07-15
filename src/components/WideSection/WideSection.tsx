8a12c7ef0238237b86caab5e42aa0b0cb83d3c09
Roman Pavlenko <proman9973@gmail.com>
2022-07-15 13:48:54 +0300

    second section completed

--- src/components/WideSection/WideSection.tsx
+++ src/components/WideSection/WideSection.tsx
@@ -4,7 +4,21 @@ import './WideSection.scss';
 export const WideSection: React.FC = () => {
   return (
     <div className="container--full wide_section">
-      <h1>hi</h1>
+      <h1 className="wide_section__heading">
+        Test assignment for front-end developer
+      </h1>
+      <p className="wide_section__paragraph">
+        What defines a good front-end developer is one that has skilled knowledge of HTML,
+        CSS, JS with a vast understanding of
+        User design thinking as they&#39;ll be building web interfaces with accessibility in mind.
+        They should also be excited to learn, as the world of Front-End Development keeps evolving.
+      </p>
+      <button
+        type="button"
+        className="button"
+      >
+        Sign up
+      </button>
     </div>
   );
 };
